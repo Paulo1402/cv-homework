7aa3d604b7e6c4b59999b3b64e42eebaccafbfef
Paulo Benatto <paulobenatto02@gmail.com>
2024-08-05 20:27:46 -0300

    Refactor code for consistency and readability

--- main.js
+++ main.js
@@ -1,14 +1,45 @@
-const contactMeForm = document.getElementById('contactMeForm');
-const nameInput = document.getElementById('nameInput');
-const emailInput = document.getElementById('emailInput');
-const messageInput = document.getElementById('messageInput');
+const contactMeForm = document.getElementById("contactMeForm")
+const nameInput = document.getElementById("nameInput")
+const emailInput = document.getElementById("emailInput")
+const messageInput = document.getElementById("messageInput")
 
-contactMeForm.addEventListener('submit', handleSubmit);
+// Adiciona um event listener para o evento "submit" do formulário
+contactMeForm.addEventListener("submit", handleSubmit)
 
-function handleSubmit() {
-    alert('Mensagem enviada!');
+// Atualiza a URL toda vez que o valor de um input muda
+nameInput.addEventListener("input", updateURL)
+emailInput.addEventListener("input", updateURL)
+messageInput.addEventListener("input", updateURL)
 
-    nameInput.value = '';
-    emailInput.value = '';
-    messageInput.value = '';
+// Atualiza a URL com os valores dos inputs
+function updateURL() {
+  const params = new URLSearchParams()
+
+  if (nameInput.value) {
+    params.set("name", nameInput.value)
+  }
+
+  if (emailInput.value) {
+    params.set("email", emailInput.value)
+  }
+
+  if (messageInput.value) {
+    params.set("message", messageInput.value)
+  }
+
+  const newUrl = `${window.location.pathname}?${params.toString()}`
+  window.history.replaceState({}, "", newUrl)
+}
+
+// Envia o formulário
+function handleSubmit(event) {
+  event.preventDefault()
+
+  alert("Mensagem enviada!")
+
+  nameInput.value = ""
+  emailInput.value = ""
+  messageInput.value = ""
+
+  updateURL()
 }
